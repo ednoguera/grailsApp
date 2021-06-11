27de4a354b77e00dfaebdb06fa4ca5fe52807495
Edson Silva <edson@purespectrum.com>
2021-06-11 15:25:54 -0300

    add domain class make and model

---
+++ grails-app/domain/org/grails/guides/Make.groovy
@@ -0,0 +1,12 @@
+package org.grails.guides
+
+class Make {
+    String name
+
+    static constraints = {
+    }
+
+    String toString() {
+        name
+    }
+}
--- grails-app/domain/org/grails/guides/Vehicle.groovy
+++ grails-app/domain/org/grails/guides/Vehicle.groovy
@@ -1,7 +1,16 @@
 package org.grails.guides
 
+@SuppressWarnings('GrailsDomainReservedSqlKeywordName')
 class Vehicle {
 
+    Integer year
+
+    String name
+    String make
+    String model
+
     static constraints = {
+        year min: 1900
+        name maxSize: 255
     }
 }
